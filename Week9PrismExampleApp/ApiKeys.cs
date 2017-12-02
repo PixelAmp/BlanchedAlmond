e4e58dd7d8709eabda947a6a3c97028c2119989e
Raul Perez <reperez79@gmail.com>
2017-12-01 16:29:39 -0800

    Added tracker API key, removed useless pages

--- Week9PrismExampleApp/ApiKeys.cs
+++ Week9PrismExampleApp/ApiKeys.cs
@@ -4,7 +4,7 @@ namespace Week9PrismExampleApp
     public class ApiKeys
     {
 		// Obtain API Key https://openweathermap.org/api
-		public const string WeatherKey = "YOUR_API_KEY_HERE";
+		public const string WeatherKey = "d649eafd-0d58-4569-a6f1-5ae013f80f66";
         public const string AnalyticsKeyiOS = "Your Key Here";
         public const string AnalyticsKeyAndroid = "Your Key Here";
     }
