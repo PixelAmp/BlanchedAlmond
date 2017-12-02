8f7e84e618529014b8eb291aeb1a81f3eca79e7e
Judah Perez <tythin4@gmail.com>
2017-12-01 16:34:59 -0800

    get the keys

--- Week9PrismExampleApp/ApiKeys.cs
+++ Week9PrismExampleApp/ApiKeys.cs
@@ -5,7 +5,7 @@ namespace Week9PrismExampleApp
     {
 		// Obtain API Key https://openweathermap.org/api
 		public const string WeatherKey = "d649eafd-0d58-4569-a6f1-5ae013f80f66";
-        public const string AnalyticsKeyiOS = "Your Key Here";
-        public const string AnalyticsKeyAndroid = "Your Key Here";
+        public const string AnalyticsKeyiOS = "ios=b3647342-6d27-4a7f-b1dd-9eb5aa93780f;";
+        public const string AnalyticsKeyAndroid = "android=00ead354-54ba-40a6-a74b-d056d7cb83c4;";
     }
 }
