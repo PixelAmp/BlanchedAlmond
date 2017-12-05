05c1fd2ef2943924ff274e8889c11cfe0e02d928
Judah Perez <tythin4@gmail.com>
2017-12-04 23:11:16 -0800

    some formatting for the user stat page

--- Week9PrismExampleApp/ViewModels/UserStatsPageViewModel.cs
+++ Week9PrismExampleApp/ViewModels/UserStatsPageViewModel.cs
@@ -12,9 +12,17 @@ namespace Week9PrismExampleApp.ViewModels
 {
     public class UserStatsPageViewModel : BindableBase
     {
+
+        private StatsResponse _statsResponse;
+        public StatsResponse StatsResponse
+        {
+            get { return _statsResponse; }
+            set { SetProperty(ref _statsResponse, value); }
+        }
+
         public UserStatsPageViewModel()
         {
-            
+            StatsResponse = LoadUserStats("cookiedragon4").Result;
         }
 
         public async Task<StatsResponse> LoadUserStats(string username)
