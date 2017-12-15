835f657b1955689ac9427683e0bce86c0a83009d
Unknown <reperez79@gmail.com>
2017-12-15 02:56:04 -0800

    FIXED (Hopefully)
    We encountered a race ondition where github freaked out when we both pushed different versions of the application. 
    Please look over your code to make sure it still works properly!

--- Week9PrismExampleApp/ViewModels/MainPageViewModel.cs
+++ Week9PrismExampleApp/ViewModels/MainPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,7 @@ using System.Collections.ObjectModel;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
 using Microsoft.AppCenter.Analytics;
+using PUBGSharp.Data;
 
 [assembly: InternalsVisibleTo("Week9PrismExampleUnitTests")]
 namespace Week9PrismExampleApp.ViewModels
@@ -16,16 +18,27 @@ namespace Week9PrismExampleApp.ViewModels
     {
         public DelegateCommand NavToNewPageCommand { get; set; }
         public DelegateCommand NavToTimerPageCommand { get; set; }
+        public DelegateCommand NavToUserStatsPageCommand { get; set; }
 
-        INavigationService _navigationService;
+        private INavigationService _navigationService;
+        private IPageDialogService _dialogService;
 
-        public MainPageViewModel(INavigationService navigationService)
+        public MainPageViewModel(INavigationService navigationService, IPageDialogService dialogService)
         {
             _navigationService = navigationService;
+            _dialogService = dialogService;
             NavToNewPageCommand = new DelegateCommand(NavToNewPage);
             NavToTimerPageCommand = new DelegateCommand(NavToTimerPage);
+            NavToUserStatsPageCommand = new DelegateCommand(NavToUserStatsPage);
         }
-        
+
+        private bool _IsLoading;
+        public bool IsLoading
+        {
+            get { return _IsLoading; }
+            set { SetProperty(ref _IsLoading, value); }
+        }
+
 
         private async void NavToNewPage()
         {
@@ -39,6 +52,26 @@ namespace Week9PrismExampleApp.ViewModels
             var navParams = new NavigationParameters();
             navParams.Add("NavFromPage", "MainPageViewModel");
             await _navigationService.NavigateAsync("TimerPage", navParams);
+        }
+
+        private async void NavToUserStatsPage()
+        {
+            IsLoading = true;
+            try
+            {
+                PUBGSharp.PUBGStatsClient statsClient = new PUBGSharp.PUBGStatsClient(ApiKeys.PUBG);
+                var stats = await statsClient.GetPlayerStatsAsync("cookiedragon4", Region.NA);
+                IsLoading = false;
+                var navParams = new NavigationParameters();
+                navParams.Add("NavFromPage", "MainPageViewModel");
+                navParams.Add("Stats", stats);
+                await _navigationService.NavigateAsync("UserStatsPage", navParams);
+            }
+            catch (Exception ex)
+            {
+                IsLoading = false;
+                await _dialogService.DisplayAlertAsync("Error", ex.Message, "OK");
+            }
         }
 
         public void OnNavigatedFrom(NavigationParameters parameters)
