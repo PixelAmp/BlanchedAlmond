b5fd2b7012056dc79246b9d19449cde0634cd68e
JamesLenz <jamesclenzini@gmail.com>
2017-12-14 22:20:23 -0800

    Revert "MainPage style, SearchUserPage style, App.xaml styes"
    This reverts commit f015b40d5a797458bf16241f55f7a49f97ead9be.

--- Week9PrismExampleApp/ViewModels/MainPageViewModel.cs
+++ Week9PrismExampleApp/ViewModels/MainPageViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
-using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,8 +9,6 @@ using System.Net.Http;
 using System.Runtime.CompilerServices;
 using Microsoft.AppCenter.Analytics;
 
-using PUBGSharp.Data;
-
 [assembly: InternalsVisibleTo("Week9PrismExampleUnitTests")]
 namespace Week9PrismExampleApp.ViewModels
 {
@@ -19,25 +16,14 @@ namespace Week9PrismExampleApp.ViewModels
     {
         public DelegateCommand NavToNewPageCommand { get; set; }
         public DelegateCommand NavToTimerPageCommand { get; set; }
-        public DelegateCommand NavToUserStatsPageCommand { get; set; }
 
-        private INavigationService _navigationService;
-        private IPageDialogService _dialogService;
+        INavigationService _navigationService;
 
-        public MainPageViewModel(INavigationService navigationService, IPageDialogService dialogService)
+        public MainPageViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
-            _dialogService = dialogService;
             NavToNewPageCommand = new DelegateCommand(NavToNewPage);
             NavToTimerPageCommand = new DelegateCommand(NavToTimerPage);
-            NavToUserStatsPageCommand = new DelegateCommand(NavToUserStatsPage);
-        }
-
-        private bool _IsLoading;
-        public bool IsLoading
-        {
-            get { return _IsLoading; }
-            set { SetProperty(ref _IsLoading, value); }
         }
         
 
@@ -54,33 +40,6 @@ namespace Week9PrismExampleApp.ViewModels
             navParams.Add("NavFromPage", "MainPageViewModel");
             await _navigationService.NavigateAsync("TimerPage", navParams);
         }
-
-        private async void NavToUserStatsPage()
-        {
-            IsLoading = true;
-            try
-            {
-                PUBGSharp.PUBGStatsClient statsClient = new PUBGSharp.PUBGStatsClient(ApiKeys.PUBG);
-                var stats = await statsClient.GetPlayerStatsAsync("cookiedragon4", Region.NA);
-                IsLoading = false;
-                var navParams = new NavigationParameters();
-                navParams.Add("NavFromPage", "MainPageViewModel");
-                navParams.Add("Stats", stats);
-                await _navigationService.NavigateAsync("UserStatsPage", navParams);
-            }
-            catch (Exception ex)
-            {
-                IsLoading = false;
-                await _dialogService.DisplayAlertAsync("Error", ex.Message, "OK");
-            }
-        }
-
-        /*private async void NavTo<page name>Page()
-        {
-            var navParams = new NavigationParameters();
-            navParams.Add("NavFromPage", "MainPageViewModel");
-            await _navigationService.NavigateAsync("<page name>", navParams);
-        }*/
 
         public void OnNavigatedFrom(NavigationParameters parameters)
         {
